5056ba72197daa28507be0c57d2b0a6ee4bce563
Cydney Pollard <sincerelycydney087@gmail.com>
2021-03-22 19:29:53 -0400

    added bootstrap CDN and navbar test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import {Navigation} from './components/Navigation.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
+
+class App extends React.Component {
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
+     <Router>
+       <Navigation />
+     </Router>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Home extends React.Component() {
+    render() {
+        return (
+            <div>
+                  <Button variant="primary">Primary</Button>{' '}
+            </div>
+        );
+    }
+}
+export default Home;
