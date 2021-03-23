496670a1e38507c8f6d78a659251a006f3d3355f
Cydney Pollard <sincerelycydney087@gmail.com>
2021-03-23 17:42:05 -0400

    changed navbar and added links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,27 +1,22 @@
 import React from "react";
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 
 
 class Navigation extends React.Component {
 
       render() {
           return (
-            <div> 
-              <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <div>
+            <Navbar expand="lg">
+            <Navbar.Brand href="#/" className="para-center">Home</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
+            <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <Nav.Link href="#/about" className="para-center"> About</Nav.Link>
+              <Nav.Link href="#/portfolio" className="para-center"> Portfolio</Nav.Link>
+              <Nav.Link href="#/skills" className="para-center">Skills</Nav.Link>
+              <Nav.Link href="#/contact" className="para-center">Contact</Nav.Link>
+            </Nav>
             </Navbar.Collapse>
           </Navbar>
           </div>
