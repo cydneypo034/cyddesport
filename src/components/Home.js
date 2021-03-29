e1ebae2329862d3bbbc28878b091032ef2171929
Cydney Pollard <sincerelycydney087@gmail.com>
2021-03-29 11:33:06 -0400

    added h2 for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import React, {Component} from "react";
 export default class Home extends Component {
     render () {
         return (
-            <h1 className="HomeFont">hello, it's nice to meet you.</h1>
+            <div>
+            <h1 className="HomeFont">hi, it's nice to meet you.</h1>
+            <h2 className="HomeFont">my name is Cydney Pollard.</h2>
+            </div>
         );
     }
 }
