d06a425194f66dba17138caa8f8d99043ecc1dee
Cydney Pollard <sincerelycydney087@gmail.com>
2021-03-29 10:48:48 -0400

    Added google font for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import react, {Component} from "react";
 export default class Home extends Component {
     render () {
         return (
-            <h1 className="Home-Bckgrnd-Image">hello, it's nice to meet you.</h1>
+            <h1 className="Home-Font">hello, it's nice to meet you.</h1>
         );
     }
 }
