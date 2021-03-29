1f5cb96dfa6e2a9e23c5b06a8535292528b8524c
Cydney Pollard <sincerelycydney087@gmail.com>
2021-03-29 15:15:22 -0400

    added hero section to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,13 @@ export default class Home extends Component {
     render () {
         return (
             <div>
-            <h1 className="HomeFont">hi, it's nice to meet you.</h1>
-            <h2 className="HomeFont">my name is Cydney Pollard.</h2>
+                    <section class="hero is-medium">
+                    <div class="hero-body">
+                    <h1 className="HomeFont">hi, it's nice to meet you.</h1>
+                    <h2 className="HomeFont">my name is Cydney Pollard.</h2>
+                    <p className="HomeSubFont">i'm currently i'm a software engineer with a background in graphic design, currently residing in Atlanta, Georgia</p>
+                    </div>
+                    </section>
             </div>
         );
     }
