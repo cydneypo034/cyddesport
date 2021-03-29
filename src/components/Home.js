3fdd1a6358e6cdc84a07ac95128995cdfd2d918c
Cydney Pollard <sincerelycydney087@gmail.com>
2021-03-29 15:29:26 -0400

    added buttons to home page, added github and cv links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {Button} from 'react-bootstrap';
 
 
 export default class Home extends Component {
@@ -9,7 +10,12 @@ export default class Home extends Component {
                     <div class="hero-body">
                     <h1 className="HomeFont">hi, it's nice to meet you.</h1>
                     <h2 className="HomeFont">my name is Cydney Pollard.</h2>
-                    <p className="HomeSubFont">i'm currently i'm a software engineer with a background in graphic design, currently residing in Atlanta, Georgia</p>
+                    <p className="HomeSubFont">i'm a software engineer with a background in graphic design, currently residing in Atlanta, Georgia</p>
+
+                    <div class="buttons">
+                    <button class="button is-warning is-rounded" href="https://github.com/cydneypo034">Github</button>
+                    <button class="button is-warning is-rounded" href="components/uploads/CydneyPollardCV2021.pdf">CV/Resume</button>
+                    </div>
                     </div>
                     </section>
             </div>
