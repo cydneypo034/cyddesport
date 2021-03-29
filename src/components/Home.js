e1b836040be2b383ffe5c9612f58b9d21545b400
Cydney Pollard <sincerelycydney087@gmail.com>
2021-03-29 17:36:47 -0400

    adding rounded circle image to about me page

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,12 +10,12 @@ export default class Home extends Component {
                     <section class="hero is-medium">
 
                     <div class="hero-body">
-                    <h1 className="HomeFont">hi, it's nice to meet you.</h1>
-                    <h2 className="HomeFont">my name is Cydney Pollard.</h2>
-                    <p className="HomeSubFont">i'm a software engineer with a 
+                    <h1 className="DocFont">hi, it's nice to meet you!</h1>
+                    <h2 className="DocFont">my name is Cydney Pollard.</h2>
+                    <p className="DocSubFont">i'm a software engineer with a 
                     background in graphic design, currently residing in Atlanta, Georgia</p>
 
-                    <div className="HomeButton">
+                    <div className="DocButton">
                     <Button variant="default" style= {{ color: 'white', background: '#D9A3A3'}} href="https://github.com/cydneypo034">Github</Button> {' '}
                     <Button variant="default" style= {{ color: 'white', background: '#D9A3A3'}} href={PDF} target="_blank">CV/Resume</Button>
                     </div>
