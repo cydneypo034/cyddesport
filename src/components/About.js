282628844f4067ec285a4abee0b1181ff324c32b
Cydney Pollard <sincerelycydney087@gmail.com>
2021-03-29 20:37:18 -0400

    center profile image in about me page

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
 import React, {Component} from "react";
 import Image from 'react-bootstrap/Image';
-import Cydney from './images/cydneyaboutmephoto.png'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Cydney from './images/cydneyaboutmephoto.png';
 
 export default class About extends Component {
     render () {
         return (
-            <section class="section is-large">
-                <div className="AboutMeImage">
-                <Image src={Cydney} roundedCircle />
-                </div>
+            <div className="container">
+                <Row className="justify-content-md-center">
+                <Col xs={12} sm={4} md={4}>
+                <Image src={Cydney} roundedCircle className="AboutMeImage"/>
+                </Col>
+                </Row>
             <h1 class="DocFont">here's my story</h1>
             <h2 class="DocSubFont">
                 I am a software engineer with an eye for Front-End Web Development. 
@@ -19,8 +23,10 @@ export default class About extends Component {
                 way I could be creative and have a satisfying career. Hence, the start of my journey as a web developer began in July of 2019.
                 Through front-end web development, I have grown to love the process of designing layouts for web applications and 
                 it challenges me to think of how to structure my coding projects, what things to consider when it comes to the
-                memory and time complexity of data, and how to figure out what real world problem I am solving.            </h2>
-            </section>
+                memory and time complexity of data, and how to figure out what real world problem I am solving.            
+            </h2>
+            </div>
+            
         )
     }
 };
