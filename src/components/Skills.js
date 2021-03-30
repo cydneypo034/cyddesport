bd7732c611747dac64b84fd04bc7686fa99d754c
Cydney Pollard <sincerelycydney087@gmail.com>
2021-03-30 17:33:57 -0400

    added languages to skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,8 +5,20 @@ export default class Skills extends Component {
     render () {
         return (
             <div>
-            <h1 className="DocFont">Skills</h1>
-            <h2 className="DocSubFont"> Here are a few of the technologies, libraries, languages and frameworks I've had the chance to worked with: </h2>
+
+                <section class="hero is-small">
+                    <div className="hero-body">
+                    <h1 className="DocFont">Skills</h1>
+                    <h2 className="DocSubFont"> Here are a few of the technologies, libraries, languages and frameworks I've had the chance to worked with: </h2>
+                    </div>
+                </section>
+
+                <div className="techs">
+                <p className="DocSmallFont">Languages: </p>
+                    <p className="DocSubSmallFont">HTML, CSS, JavaScript(ES6), Python, SQL, Java</p>
+                </div>
+
+
             </div>
            
         )
