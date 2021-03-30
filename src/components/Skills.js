e481a2b3d9eb5741c33b8ee8ab40c1b37a8bc065
Cydney Pollard <sincerelycydney087@gmail.com>
2021-03-30 17:41:49 -0400

    completed skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,11 +13,41 @@ export default class Skills extends Component {
                     </div>
                 </section>
 
-                <div className="techs">
-                <p className="DocSmallFont">Languages: </p>
+                <section className="hero is-small">
+                    <div className="hero-body">
+                    <p className="DocSmallFont">Languages</p>
                     <p className="DocSubSmallFont">HTML, CSS, JavaScript(ES6), Python, SQL, Java</p>
-                </div>
+                    </div>
+               
+                </section>
 
+                <section className="hero is-small">
+                    <div className="hero-body">
+                    <p className="DocSmallFont">Front End Frameworks</p>
+                    <p className="DocSubSmallFont">React.js, React Redux, React Router DOM, Reselect</p>
+                    </div>
+                </section>
+
+                <section className="hero is-small">
+                    <div className="hero-body">
+                    <p className="DocSmallFont">Back End Techologies</p>
+                    <p className="DocSubSmallFont">Mongoose, Node.js, Express.js, Django Rest Framework</p>
+                    </div>
+                </section>
+
+                <section className="hero is-small">
+                    <div className="hero-body">
+                    <p className="DocSmallFont">Front End Libraries</p>
+                    <p className="DocSubSmallFont">Bulma, Bootstrap, Material Design Bootstrap, Material U</p>
+                    </div>
+                </section>
+
+                <section className="hero is-small">
+                    <div className="hero-body">
+                    <p className="DocSmallFont">Coding Tools and Platforms</p>
+                    <p className="DocSubSmallFont">Git Version Control, Visual Studio Code, Github, Heroku, Netlify, Figma, Trello</p>
+                    </div>
+                </section>
 
             </div>
            
