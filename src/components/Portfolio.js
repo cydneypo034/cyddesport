721d1a1eb4d7b49eccb11a658c62be0e03e45b02
Cydney Pollard <sincerelycydney087@gmail.com>
2021-03-30 14:20:06 -0400

    added images to image folder and added links to first project in portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import CrownClothes from '../components/images/crownclothesproj.png';
+import GitHubLogo from '../components/images/githublogo.png';
+import HerokuLogo from '../components/images/heroku.png';
 
 export default class Portfolio extends Component {
     render () {
@@ -13,11 +15,25 @@ export default class Portfolio extends Component {
                 
                 <div className="card">
                 <img src={CrownClothes} alt="clothing-store-website" className="Portfolio-Image" />
+                <div className="card-content">
+                    <div className="media-content">
+                        <p className="title is-4">CRWN Clothing Ltd.</p>
+                    </div>
+                    <div className="content">
+                    CRWN Clothing Ltd. is a e-commerce fashion website made with React, React Router DOM, React Redux, Stripe API, Firebase, and Reselect. 
+                    &nbsp; <a href="https://github.com/cydneypo034/crown-clothing"><img src={GitHubLogo} alt="github-logo" height="50px" width="50px"/></a>
+                    <a href="https://crownclothes.herokuapp.com"><img src={HerokuLogo} alt="heroku-logo" height="50px" width="50px"/></a>
+                    </div>
+                </div>
+                </div>
+
+
+                <div className="card">
+                <img src={CrownClothes} alt="clothing-store-website" className="Portfolio-Image" />
                 <div className="overlay">
 
                 </div>
                 </div>
-                
                 
                 
                 </div>
