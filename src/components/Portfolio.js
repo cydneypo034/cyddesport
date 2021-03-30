723a23a2f3c3173a4725e88222750a8c9a119705
Cydney Pollard <sincerelycydney087@gmail.com>
2021-03-29 22:06:29 -0400

    added card wrapper for cards in portfolio section:

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,18 +6,21 @@ export default class Portfolio extends Component {
         return (
             <div>
                 <h1 className="DocFont">my portfolio</h1>
-                <h2 className="DocSubFont">below is a list of my web applications I've completed from July 2019 until now.</h2>
+                <h2 className="DocSubFont">Below is a list of my web applications I've completed from July 2019 until now.</h2>
             
             <section>
-            <div className="card">
+                <div className="card-wrapper">
+                
+                <div className="card">
                 <img src={CrownClothes} alt="clothing-store-website" className="Portfolio-Image" />
                 <div className="overlay">
-                    
+
                 </div>
-
-            </div>
-
-           
+                </div>
+                
+                
+                
+                </div>
             </section>
             </div>
         )
