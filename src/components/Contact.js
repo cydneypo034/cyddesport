59983f6f0522c4f0c7903cf30d61585e279f6aac
Cydney Pollard <sincerelycydney087@gmail.com>
2021-04-23 17:02:11 -0400

    added new contact form from bulma

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,73 +3,27 @@ import {Button, Form, Row, Col, InputGroup} from "react-bootstrap";
 import{ init } from 'emailjs-com';
 
 export default class Contact extends Component {
-    state = {
-        name = '',
-        email = '',
-        subject = '',
-        message = ''
-    }
 
-    handleSubmit(e) {
-        e.preventDefault()
-
-        const { name, email, subject, message } = this.state
-
-
-
-    }
 
     render () {
         return (
             <div>
-            <h1 className="DocFont">Contact</h1>
-            <h2 className="DocSubFont">Here are several ways to get in contact with me:</h2>
-            
-            <Form>
-            <Form.Group as={Row} controlId="formHorizontalName">
-                <Form.Label column sm={2}>
-                Name
-                </Form.Label>
-                <Col sm={10}>
-                <Form.Control type="name" placeholder="Name" />
-                </Col>
-            </Form.Group>
+                <div class="field">
+                <label class="label">Name</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="Text input"/>
+                </div>
+                </div>
 
-            <Form.Group as={Row} controlId="formHorizontalEmail">
-                <Form.Label column sm={2}>
-                Email
-                </Form.Label>
-                <Col sm={10}>
-                <Form.Control type="email" placeholder="Email" />
-                </Col>
-            </Form.Group>
-            
-            <Form.Group as={Row} controlId="formHorizontalSubject">
-                <Form.Label column sm={2}>
-                Subject
-                </Form.Label>
-                <Col sm={10}>
-                <Form.Control type="subject" placeholder="Subject" />
-                </Col>
-            </Form.Group>
+                <div class="field">
+                <label class="label">Subject</label>
+                <div class="control has-icons-left has-icons-right">
+                    <input class="input is-success" type="text" placeholder="Text input" value="bulma"/>
+                </div>
+                </div>
 
-            <Form.Group as={Row} controlId="formHorizontalMessage">
-                <Form.Label column sm={2}>
-                Message
-                </Form.Label>
-                <Col sm={10}>
-                <InputGroup as="textarea" aria-label="With textarea" type="textarea" placeholder="Type Your Message Here" />
-                </Col>
-            </Form.Group>
-
-            <Form.Group as={Row}>
-                <Col sm={{ span: 10, offset: 2 }}>
-                <Button type="submit" variant="default" style= {{ color: 'white', background: '#D9A3A3'}}>Submit</Button>
-                </Col>
-            </Form.Group>
-            </Form>
-            
             </div>
+            
         )
     }
 };
