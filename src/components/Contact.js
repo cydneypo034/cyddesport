8d3f16318225513c25e3c4ff9a404f9f682ee34c
Cydney Pollard <sincerelycydney087@gmail.com>
2021-04-23 17:10:48 -0400

    added bulma form components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,9 +16,35 @@ export default class Contact extends Component {
                 </div>
 
                 <div class="field">
+                <label class="label">Email</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="Text input"/>
+                </div>
+                </div>
+
+                <div class="field">
                 <label class="label">Subject</label>
-                <div class="control has-icons-left has-icons-right">
-                    <input class="input is-success" type="text" placeholder="Text input" value="bulma"/>
+                <div class="control">
+                    <div class="select">
+                    <select>
+                        <option>Select dropdown</option>
+                        <option>Networking Opportunity</option>
+                        <option>Quick Zoom Chat</option>
+                    </select>
+                    </div>
+                </div>
+                </div>
+
+                <div class="field">
+                <label class="label">Message</label>
+                <div class="control">
+                    <textarea class="textarea" placeholder="Textarea"></textarea>
+                </div>
+                </div>
+
+                <div class="field">
+                <div class="control">
+                    <button class="button is-link">Submit</button>
                 </div>
                 </div>
 
