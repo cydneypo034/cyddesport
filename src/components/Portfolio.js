e5a5e6afb526d60a337598010aee9b73dd53c400
Cydney Pollard <sincerelycydney087@gmail.com>
2021-04-26 18:11:04 -0400

    created divs for social media icons in portfolio file

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -32,12 +32,13 @@ export default class Portfolio extends Component {
                     <div className="content">
                     CRWN Clothing Ltd. is a e-commerce fashion website made with
                      React, React Router DOM, React Redux, Stripe API, Firebase, and Reselect. 
-                    &nbsp; <a href="https://github.com/cydneypo034/crown-clothing"><img src={GitHubLogo} alt="github-logo" height="50px" width="50px"/></a>
+                     <div className="SocialMedia-Icons">
+                    <a href="https://github.com/cydneypo034/crown-clothing"><img src={GitHubLogo} alt="github-logo" height="50px" width="50px"/></a>
                     <a href="https://crownclothes.herokuapp.com"><img src={HerokuLogo} alt="heroku-logo" height="50px" width="50px"/></a>
                     </div>
+                    </div>
                 </div>
-                
-                   </div>
+                </div>
                    
                 </div>
                </div>
@@ -45,6 +46,9 @@ export default class Portfolio extends Component {
 
                <div className="column">
                 <div className="card">
+                <img src={FiaworldPage} alt="website-with-three-artists-and-FIA-company-description" className="Portfolio-Image" />
+
+                <div className="overlay">
                 <div className="card-content">
                     <div className="media-content">
                         <p className="title is-4">Fiaworld Productions</p>
@@ -52,53 +56,71 @@ export default class Portfolio extends Component {
                     <div className="content">
                     Fiaworld.com is a production company website made with 
                     React, React Router DOM, Python, SQL, Django Rest Framework and Express.Js.
-                    &nbsp; <a href="https://github.com/cydneypo034/fiaworld-project"><img src={GitHubLogo} alt="github-logo" height="50px" width="50px"/></a>
+                    <div className="SocialMedia-Icons">
+                   <a href="https://github.com/cydneypo034/fiaworld-project"><img src={GitHubLogo} alt="github-logo" height="50px" width="50px"/></a>
                     <a href="https://fierce-mountain-28176.herokuapp.com"><img src={HerokuLogo} alt="heroku-logo" height="50px" width="50px"/></a>
                     </div>
-
+                    </div>
                 </div>
+                </div>
+                
                 </div>
                 </div>
 
 
                 <div className="column">
                 <div className="card">
+                <img src={Feminaku} alt="website-page-with-anime-girl" className="Portfolio-Image" />
+
+                <div className="overlay">
                 <div className="card-content">
                     <div className="media-content">
                         <p className="title is-4">Feminaku Social Database</p>
                     </div>
                     <div className="content">
                     Feminaku Social Database is an social media application made with 
-                    React, React Router DOM, Express.Js, MDBootstrap, Mongoose and Mongo DB.
-                     &nbsp; <a href="https://github.com/cydneypo034/feminakutwo"><img src={GitHubLogo} alt="github-logo" height="50px" width="50px"/></a>
+                    React, React Router DOM, Mongoose, MDBootstrap, and Mongo DB.
+                    <div className="SocialMedia-Icons">
+                    <a href="https://github.com/cydneypo034/feminakutwo"><img src={GitHubLogo} alt="github-logo" height="50px" width="50px"/></a>
                     <a href="https://feminaku.herokuapp.com/"><img src={HerokuLogo} alt="heroku-logo" height="50px" width="50px"/></a>
                     </div>
-
+                    </div>
                 </div>
+                </div>
+               
                 </div>
                 </div>
 
 
                 <div className="column">
                 <div className="card">
+                <img src={ArtistPocket} alt="website-with-paintbrush-strokes" className="Portfolio-Image" />
+
+                <div className="overlay">
                 <div className="card-content">
                     <div className="media-content">
                         <p className="title is-4">The Artist's Pocket Database</p>
                     </div>
                     <div className="content">
-                    An Artist's Pocket Database is an art store directory application 
-                    made with Express.Js, Handlebars.Js, Mongoose, Mongo DB and Bootstrap.
-                    &nbsp; <a href="https://github.com/cydneypo034/artist-app"><img src={GitHubLogo} alt="github-logo" height="50px" width="50px"/></a>
+                    An Artist's Pocket Database is an art store directory
+                    made with Handlebars.Js, Mongoose, Mongo DB and Bootstrap.
+                    <div className="SocialMedia-Icons">
+                    <a href="https://github.com/cydneypo034/artist-app"><img src={GitHubLogo} alt="github-logo" height="50px" width="50px"/></a>
                     <a href="https://vast-dusk-81442.herokuapp.com/artstore"><img src={HerokuLogo} alt="heroku-logo" height="50px" width="50px"/></a>
                     </div>
-
+                    </div>
                 </div>
+                </div>
+               
                 </div>
                 </div>
 
 
                 <div className="column">
                 <div className="card">
+                <img src={MySimonSays} alt="site-with-ponies-in-sqaures" className="Portfolio-Image" />
+
+                <div className="overlay">
                 <div className="card-content">
                     <div className="media-content">
                         <p className="title is-4">My Best Friend Says</p>
@@ -106,11 +128,14 @@ export default class Portfolio extends Component {
                     <div className="content">
                     My Best Friend Says is a rendition of the Simon Says game featuring My Little Pony 
                     characters made with HTML, CSS and ES6 JavaScript.
-                    &nbsp; <a href="https://github.com/cydneypo034/mybestfriendsays"><img src={GitHubLogo} alt="github-logo" height="50px" width="50px"/></a>
+                    <div className="SocialMedia-Icons">
+                    <a href="https://github.com/cydneypo034/mybestfriendsays"><img src={GitHubLogo} alt="github-logo" height="50px" width="50px"/></a>
                     <a href="https://adoring-bhaskara-ff6b52.netlify.com/"><img src={netlify} alt="heroku-logo" height="50px" width="50px"/></a>
                     </div>
-
+                    </div>
                 </div>
+                </div>
+                
                 </div>
                 </div>
 
