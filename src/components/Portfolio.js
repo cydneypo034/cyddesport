d811358571c1828ec7cc99f3744d478082e1bc1b
Cydney Pollard <sincerelycydney087@gmail.com>
2021-12-29 22:45:01 -0500

    added studio ghibli project and image

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import CydneyPortfolio from '../components/images/cydneyportfolioimage.png';
 import CrunchyrollPage from '../components/images/crunchyroll-loginpage.png';
-import CrownClothing from '../components/images/crownclothingapp.png';
-import JumpGame from '../components/images/thejumpgame.png';
 import AnimeBae from '../components/images/animebaephoto.png';
+import GhibliClock from '../components/images/ghibliclock.png';
 import GitHubLogo from '../components/images/githublogo.png';
 import HerokuLogo from '../components/images/heroku.png';
 import NetlifyLogo from '../components/images/logomark.png';
@@ -18,6 +17,29 @@ export default class Portfolio extends Component {
             <section>
                 
                 <div className="columns is-multiline">
+
+                <div className="column">
+                <div className="card">
+                <img src={GhibliClock} alt="ghibli-movie-slideshow" className="Portfolio-Image" />
+
+                <div className="overlay">
+                <div className="card-content">
+                    <div className="media-content">
+                        <p className="title is-4">Ghibli Clock App</p>
+                    </div>
+                    <div className="content">
+                    The Ghibli Clock is a clock app with a slideshow of Studio Ghibli movies built with HTML, CSS, and JavaScript.
+                    <div className="SocialMedia-Icons">
+                    <a href="https://github.com/cydneypo034/myghibliclock"><img src={GitHubLogo} alt="github-logo" height="50px" width="50px"/></a>
+                    <a href="https://myghibliclock.netlify.app/"><img src={NetlifyLogo} alt="heroku-logo" height="50px" width="50px"/></a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+               
+                </div>
+                </div>
+
 
                 <div className="column">
                 <div className="card">
@@ -78,9 +100,8 @@ export default class Portfolio extends Component {
                         <p className="title is-4">Crunchyroll Login Page</p>
                     </div>
                     <div className="content">
-                    The Crunchyroll Sign In Page Clone is a project utilizing HTML, 
-                    CSS and vanilla JavaScript to compose a clone version of the sign up account 
-                    page for Crunchyroll, an anime streaming service.
+                    The Crunchyroll Sign In Page is a clone project based on 
+                    Crunchyroll.com's sign-in page built with HTML,<img src={NetlifyLogo} alt="heroku-logo" height="50px" width="50px"/> CSS and JavaScript.
                     <div className="SocialMedia-Icons">
                     <a href="https://github.com/cydneypo034/crunchyrolllogin"><img src={GitHubLogo} alt="github-logo" height="50px" width="50px"/></a>
                     <a href="https://crunchyroll-login.netlify.app/"><img src={NetlifyLogo} alt="heroku-logo" height="50px" width="50px"/></a>
